fb3745ffde4a1fcf2c8ca63c50465bd7cc931aff
Nguyen Thanh <ngocdanthanhdt@tvu.edu.vn>
2026-09-25 14:43:56 +0700

    Add admin order management

--- src/includes/admin/navbar.php
+++ src/includes/admin/navbar.php
@@ -43,7 +43,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="/orders/">
+                    <a class="nav-link" href="/admin/orders/">
                         Đơn hàng
                     </a>
                 </li>
